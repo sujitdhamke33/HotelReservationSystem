9f137772ab8901f7081baf11eff83127a9bc931e
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-29 10:37:03 +0530

    added weekday and weekend rates for each Hotel

--- src/main/java/org/example/HotelSystem.java
+++ src/main/java/org/example/HotelSystem.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -9,41 +10,32 @@ import java.util.List;
 import java.util.Scanner;
 
 class Hotel {
-
-    private  String name;
-    private  int rates;
-
-    public Hotel(String name, int rates) {
+    private String name;private int weekdayRate,weekendRate;
+    public Hotel(String name, int weekdayRate, int weekendRate) {
         this.name = name;
-        this.rates = rates;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
     }
-
-    public int getRates() {
-        return rates;
+    public int getRate(DayOfWeek dayOfWeek) {
+        return (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) ? weekendRate : weekdayRate;
     }
-
-
     @Override
     public String toString() {
         return "Hotel{" +
                 "name='" + name + '\'' +
-                ", rates=" + rates +
+                ", weekdayRate=" + weekdayRate +
+                ", weekendRate=" + weekendRate +
                 '}';
     }
 }
 
 public class HotelSystem {
-
     static List<Hotel> hotelList = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
-    static void addHotel (){
-        Scanner sc = new Scanner(System.in);
-        Hotel hotel1 = new Hotel("Lakewood",110);
-        hotelList.add(hotel1);
-        Hotel hotel2 = new Hotel("Bridgewood",160);
-        hotelList.add(hotel2);
-        Hotel hotel3 = new Hotel("Ridgewood",210);
-        hotelList.add(hotel3);
+    static void addHotel() {
+        hotelList.add(new Hotel("Lakewood", 110, 90));
+        hotelList.add(new Hotel("Bridgewood", 150, 50));
+        hotelList.add(new Hotel("Ridgewood", 220, 150));
         System.out.println(hotelList);
     }
 
@@ -58,19 +50,27 @@ public class HotelSystem {
         HotelSystem.hotelRateCalculate(dateStart,dateEnd,hotelList);
     }
 
-    static long hotelRateCalculate (LocalDate dateStart , LocalDate dateEnd, List<Hotel> hotelList){
-      Hotel cheapestHotel = hotelList.get(0);
-      long lowestPrice = Long.MAX_VALUE;
-      long countDay = ChronoUnit.DAYS.between(dateStart,dateEnd);
+    static long hotelRateCalculate(LocalDate startDate, LocalDate endDate, List<Hotel> hotelList) {
+        Hotel cheapestHotel = hotelList.get(0);
+        long lowestPrice = Long.MAX_VALUE;
 
-      for(Hotel hotel : hotelList){
-          long totalCost = countDay * hotel.getRates();
-          System.out.println("Total cost : " + totalCost);
-          if(totalCost < lowestPrice){
-              lowestPrice = totalCost;
-              cheapestHotel = hotel;
-          }
-      }
+        for (Hotel hotel : hotelList) {
+
+            long totalCost = 0;
+
+            LocalDate currentDate = startDate;
+
+            while (!currentDate.isAfter(endDate)) {
+
+                totalCost += hotel.getRate(currentDate.getDayOfWeek());
+                currentDate = currentDate.plusDays(1);
+            }
+            if (totalCost < lowestPrice) {
+                lowestPrice = totalCost;
+                cheapestHotel = hotel;
+            }
+
+        }
         System.out.println("Cheapest Hotel : " + cheapestHotel.toString() + " and price is: " + lowestPrice);
         return lowestPrice ;
     }
--- src/test/java/org/example/HotelSystemTest.java
+++ src/test/java/org/example/HotelSystemTest.java
@@ -7,36 +7,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HotelSystemTest {
-    List<Hotel> hotelList = new ArrayList<>();
-    @Test
-    void HotelListTest() {
-
-        Hotel hotel1 = new Hotel("Lakewood",110);
-        hotelList.add(hotel1);
-        Hotel hotel2 = new Hotel("Bridgewood",160);
-        hotelList.add(hotel1);
-        Hotel hotel3 = new Hotel("Ridgewood",210);
-        hotelList.add(hotel1);
-
-        System.out.println(hotelList);
-
-        Assertions.assertEquals(3,hotelList.size());
-    }
 
     @Test
     void rateCalculator(){
-        List<Hotel> hotelList = new ArrayList<>();
-        hotelList.add(new Hotel("Lakewood", 110));
-
         LocalDate startDate = LocalDate.of(2024, 3, 1);
         LocalDate endDate = LocalDate.of(2024, 3, 5);
 
+        List<Hotel> hotelList = new ArrayList<>();
+        hotelList.add(new Hotel("Lakewood", 110, 90));
+        hotelList.add(new Hotel("Bridgewood", 150, 50));
+        hotelList.add(new Hotel("Ridgewood", 220, 150));
+
         long result = HotelSystem.hotelRateCalculate(startDate,endDate,hotelList);
         System.out.println(result);
-        Assertions.assertEquals(440, result);
+        Assertions.assertEquals(510, result);
 
     }
 }
